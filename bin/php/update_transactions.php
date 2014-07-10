cc6423580deaed436539f2200fe685dd4647ae0d
Serhey Dolgushev <dolgushev.serhey@gmail.com>
2014-07-11 00:50:37 +0300

    update_transactions

---
+++ bin/php/update_transactions.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @package MultiSafepay
+ * @author  Serhey Dolgushev <dolgushev.serhey@gmail.com>
+ * @date    11 Jul 2014
+ * */
+ini_set( 'memory_limit', '1024M' );
+
+require 'autoload.php';
+$cli = eZCLI::instance();
+
+$scriptSettings                   = array();
+$scriptSettings['description']    = 'Update status for MSP transactions';
+$scriptSettings['use-session']    = true;
+$scriptSettings['use-modules']    = true;
+$scriptSettings['use-extensions'] = true;
+
+$script  = eZScript::instance( $scriptSettings );
+$script->startup();
+$script->initialize();
+$options = $script->getOptions(
+    '', '', array()
+);
+
+
+$timestamp = microtime( true );
+
+$cli->output( 'Fetching MSP transactions' );
+
+$transactions = MultiSafepayTransaction::fetchList( array( 'status' => 'initialized' ) );
+$count        = count( $transactions );
+$defaultHost  = eZINI::instance()->variable( 'SiteSettings', 'SiteURL' );
+foreach( $transactions as $k => $transaction ) {
+    $memoryUsage = number_format( memory_get_usage( true ) / ( 1024 * 1024 ), 2 );
+    $message     = '[' . date( 'c' ) . '] ' . number_format( $k / $count * 100, 2 )
+        . '% (' . $k . '/' . $count . '), Memory usage: ' . $memoryUsage . ' Mb';
+    $cli->output( $message );
+
+    $transaction->updateStatus();
+    $cli->output( 'Transaction #' . $transaction->attribute(  't_id' ) . ': ' . $transaction->attribute( 'status' ) );
+    
+    if( $transaction->attribute( 'status' ) == 'completed' ) {
+        $order = $transaction->attribute( 'order' );
+        if( $order instanceof eZOrder === false ) {
+            continue;
+        }
+
+        // Get orders siteaccess
+        $sa    = null;
+        $items = eZOrderItem::fetchListByType( $order->attribute( 'id' ), 'siteaccess' );
+        if( count( $items ) > 0 ) {
+            $sa = $items[0]->attribute( 'description' );
+        }
+        // Set host of the order`s siteaccess
+        if( $sa === null ) {
+            $host = $defaultHost;
+        } else {
+            $host = eZINI::getSiteAccessIni( $sa, 'site.ini' )->variable( 'SiteSettings', 'SiteURL' );
+        }
+        eZINI::instance()->setVariable( 'SiteSettings', 'SiteURL', $host );
+        // Set SA path
+        $tmp                         = explode( '/', $host );
+        eZSys::instance()->IndexFile = '/' . end( $tmp );
+
+        $transaction->updatePaymentObject();
+    }
+}
+
+$cli->output( 'Finished in ' . number_format( microtime( true ) - $timestamp, 2 ) . ' sec.' );
+$script->shutdown( 0 );
